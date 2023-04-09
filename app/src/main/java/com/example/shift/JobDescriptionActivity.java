22cd280bec8ea29a82efbb3858a5d59b11a5f1f5
HylianWarrior1034 <dm10142@gmail.com>
2023-04-09 14:15:25 -0400

    Made the popup xml file

--- app/src/main/java/com/example/shift/JobDescriptionActivity.java
+++ app/src/main/java/com/example/shift/JobDescriptionActivity.java
@@ -7,10 +7,15 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.navigation.NavController;
@@ -72,6 +77,35 @@ public class JobDescriptionActivity extends AppCompatActivity {
         description.setText(currJob.getDescription());
         role.setText(currJob.getRole());
     }
+
+//    public void onButtonShowPopupWindowClick(View view) {
+//
+//        // inflate the layout of the popup window
+//        LayoutInflater inflater = (LayoutInflater)
+//                getSystemService(LAYOUT_INFLATER_SERVICE);
+//        View popupView = inflater.inflate(R.layout.popup, null);
+//
+//        // create the popup window
+//        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//        boolean focusable = true; // lets taps outside the popup also dismiss it
+//        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//
+//        // show the popup window
+//        // which view you pass in doesn't matter, it is only used for the window tolken
+//        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+//
+//        Button leaveAnywaysButton = (Button) findViewById(R.id.leave)
+//
+//        // dismiss the popup window when touched
+//        popupView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                popupWindow.dismiss();
+//                return true;
+//            }
+//        });
+//    }
 
     public void goBack(View view) {
         Intent intent = new Intent(JobDescriptionActivity.this, HomeActivity.class);
