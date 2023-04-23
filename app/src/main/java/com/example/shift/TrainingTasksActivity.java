8c4a429debcef431aeea4e723aa12df6be5147f2
HylianWarrior1034 <dm10142@gmail.com>
2023-04-23 13:05:57 -0400

    Created trainingTasksactivity

--- app/src/main/java/com/example/shift/TrainingTasksActivity.java
+++ app/src/main/java/com/example/shift/TrainingTasksActivity.java
@@ -3,12 +3,25 @@ package com.example.shift;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+
+import com.example.shift.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TrainingTasksActivity extends AppCompatActivity {
 
+    private ActivityHomeBinding binding;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_training_tasks);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 }
